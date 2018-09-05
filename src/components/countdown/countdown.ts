30574581757a36d8959e55ae4535667e43298845
Lonny Gomes <code@lonnygomes.com>
2018-09-05 18:00:20 -0400

    Change selector from countdown to app-countdown

--- src/components/countdown/countdown.ts
+++ src/components/countdown/countdown.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
  * Components.
  */
 @Component({
-  selector: 'countdown',
+  selector: 'app-countdown',
   templateUrl: 'countdown.html'
 })
 export class CountdownComponent {
