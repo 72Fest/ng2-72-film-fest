fb5a68e440db2b1d60664f0969b734f0dfb6e86e
Lonny Gomes <code@lonnygomes.com>
2018-09-05 22:58:46 -0400

    Add observable interval to update counters

--- src/components/countdown/countdown.ts
+++ src/components/countdown/countdown.ts
@@ -1,6 +1,7 @@
 import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { TimestampModel } from '../../models/timestamp.model';
 
+import { Observable, Subscription } from 'rxjs';
 /**
  * Generated class for the CountdownComponent component.
  *
@@ -15,6 +16,9 @@ export class CountdownComponent implements OnChanges {
 
   @Input() title: string = 'Loading ...';
   @Input() timestamp: TimestampModel;
+
+  timer:Observable<TimestampModel>;
+  timerSubscription:Subscription;
 
   days: string = '--';
   hours: string = '--';
@@ -63,7 +67,12 @@ export class CountdownComponent implements OnChanges {
     return timeLeft;
   }
 
-  updateCountdown(curTimestamp){
+  /**
+   * Calculate the days, hours, minutes and seconds left given a specified timestamp
+   * @param {TimestampModel} curTimestamp TimestampModel current timestamp retrieved from API
+   * @returns TimestampModel differences in time or undefined if expired
+   */
+  updateCountdown(curTimestamp:TimestampModel){
     const timeDiff:TimestampModel = this.calcTimeDiff(curTimestamp);
 
     // update countdown
@@ -73,6 +82,34 @@ export class CountdownComponent implements OnChanges {
     this.hours = timeDiff.day ? String(timeDiff.hour) : '00';
     this.minutes = timeDiff.day ? String(timeDiff.minute) : '00';
     this.seconds = timeDiff.day ? String(timeDiff.second) : '00';
+
+    return timeDiff;
+  }
+
+  /**
+   * Initializes and subscribes to an interval observer to update the counter
+   * @param {TimestampModel} curTimestamp
+   */
+  initTimer(curTimestamp:TimestampModel) {
+    // first update counts to calc differences
+    const timeDiff:TimestampModel = this.updateCountdown(curTimestamp);
+
+    // we have a valid timestamp, lets update it
+    if (this.timestamp && !isNaN(timeDiff.second)) {
+
+      // if a subscription already exists, unsubscribe first
+      if (this.timerSubscription) {
+        this.timerSubscription.unsubscribe();
+      }
+
+      // create observable that fires every second
+      this.timer = Observable.interval(1000)
+        .map(()=> this.calcTimeDiff(this.timestamp));
+
+      // subscribe to timer observable to update counter
+      this.timerSubscription = this.timer
+        .subscribe((ts) => this.updateCountdown(ts));
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -80,7 +117,8 @@ export class CountdownComponent implements OnChanges {
     if (changes.timestamp && changes.timestamp.currentValue) {
       const curTimestamp:TimestampModel = changes.timestamp.currentValue;
 
-      this.updateCountdown(curTimestamp);
+      // start the timer upon receiving the current timestamp
+      this.initTimer(curTimestamp);
     }
 
     // update title on change
