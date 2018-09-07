faf26ae9422fc63e589738069067d6adbc40c0a1
Lonny Gomes <code@lonnygomes.com>
2018-09-07 14:02:27 -0400

    Display human readable timestamp in photo card, #5

--- src/models/photo-item.model.ts
+++ src/models/photo-item.model.ts
@@ -1,20 +1,31 @@
 import { Deserializable } from '../interfaces/deserializeable.interface';
+import * as moment from 'moment';
 
 export class PhotoItemModel implements Deserializable {
   id: string;
   photoUrl: string;
   thumbUrl: string;
   timestamp: string;
+  timestampStr: string;
   isRejected: boolean;
   isFilmHour: boolean;
   filmHour: boolean;
   votes: number = 0;
+
+  private _updateTimestampStr(): void {
+    if (this.timestamp) {
+      this.timestampStr = moment(this.timestamp).fromNow();
+    }
+  }
 
   deserialize(input: any): this {
     Object.assign(this, input);
 
     this.isFilmHour = input.isFilmHour || false;
     this.filmHour = input.filmHour || '';
+
+    // update human readable timestamp
+    this._updateTimestampStr();
 
     return this;
   }
