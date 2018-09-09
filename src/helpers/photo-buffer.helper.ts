664140b5256124a3259d29f03651e7b5ca9fba09
Lonny Gomes <code@lonnygomes.com>
2018-09-08 21:40:45 -0400

    Add photo buffer helper class for infinite scroll, #5
    This class leverages observables to return lists of photos back in 10 chunk intervals

---
+++ src/helpers/photo-buffer.helper.ts
@@ -0,0 +1,71 @@
+import { PhotosModel } from '../models/photos.model';
+import { PhotoItemModel } from '../models/photo-item.model';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+
+const BUFFER_CHUNKS = 10;
+
+export class PhotoBuffer {
+  // initialized values
+  private _bufferedPhotos: PhotoItemModel[] = [];
+  private _photoChunks: PhotoItemModel[][] = [];
+
+  // instance variables
+  private _allPhotos: PhotoItemModel[];
+  private _photos$: Observable<PhotoItemModel[]>;
+  private _photosSubscription: Subscription;
+  private _subject$: BehaviorSubject<PhotoItemModel[]>;
+  private _subjectResults$: Observable<PhotoItemModel[]>;
+
+  constructor(private photosModel: PhotosModel) {
+    // configure subject
+    this._subject$ = new BehaviorSubject([]);
+    this._subjectResults$ = this._subject$.scan(
+      (finalChunk: PhotoItemModel[], curChunk: PhotoItemModel[]) => {
+        // once we receive an undefined item, we should stop concating
+        if (!curChunk) {
+          return undefined;
+        }
+
+        // merge both arrays together
+        return <PhotoItemModel[]>finalChunk.concat(curChunk);
+      }
+    );
+
+    // chunk photos
+    this._allPhotos = photosModel.message.photos;
+    this._photos$ = Observable.of(photosModel)
+      .flatMap(model => model.message.photos)
+      .bufferCount(BUFFER_CHUNKS)
+      .do((curChunk: PhotoItemModel[]) => {
+        // store photo chunks for later on demand use
+        this._photoChunks.push(curChunk);
+      });
+
+    this._photosSubscription = this._photos$.last().subscribe(finalVal => {
+      // emit the first photo chunk to the subject
+      const chunk = this._photoChunks.shift();
+      this._subject$.next(chunk);
+
+      // we no long need this subscription
+      //this._photosSubscription.unsubscribe();
+    });
+  }
+
+  subscribe(callback: (m: PhotoItemModel[]) => void): Subscription {
+    return this._subjectResults$.subscribe(callback);
+  }
+
+  fetchNext() {
+    const nextChunk = this._photoChunks.shift();
+
+    if (nextChunk) {
+      // if there still are chunks emit them
+      this._subject$.next(nextChunk);
+    } else {
+      // otherwise complete the observable
+      this._subject$.complete();
+    }
+
+    return this._subjectResults$.toPromise();
+  }
+}
