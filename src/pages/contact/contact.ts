db1d8ede2965762e795a218e9b1ced25d7815b54
Lonny Gomes <code@lonnygomes.com>
2018-09-09 21:43:31 -0400

    Use an iframe to display contact form, fix #10
    Removes references to in app browser and leverages the loading dialog to provide user feedback upon when everything is complete.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { InAppBrowser } from '@ionic-native/in-app-browser';
-
-/**
- * Generated class for the ContactPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  @ViewChild('contactFrame')
+  contactFrame: ElementRef;
+
+  destUrl: string = 'https://72fest.com/contact/';
+  safeDestUrl: SafeResourceUrl;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private iab: InAppBrowser
+    public sanitizer: DomSanitizer,
+    public loadingCtrl: LoadingController
   ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
-    const browser = this.iab.create('https://ionicframework.com/');
+    // sanitize URL
+    this.safeDestUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.destUrl);
+
+    // create loading dialog and display it until site is loaded
+    const loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    // listen for site to finish loading
+    this.contactFrame.nativeElement.addEventListener('load', event => {
+      // hide loading dialog after site is finished
+      loading.dismiss();
+    });
   }
 }
