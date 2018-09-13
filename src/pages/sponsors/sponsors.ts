f47b3b02a53494e2215c412e3bd47ddb05509b66
Lonny Gomes <code@lonnygomes.com>
2018-09-12 23:22:20 -0400

    Load sponsors page into an iframe, fix #9

--- src/pages/sponsors/sponsors.ts
+++ src/pages/sponsors/sponsors.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 /**
  * Generated class for the SponsorsPage page.
@@ -10,15 +11,38 @@ import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-sponsors',
-  templateUrl: 'sponsors.html',
+  templateUrl: 'sponsors.html'
 })
 export class SponsorsPage {
+  @ViewChild('sponsorsFrame')
+  sponsorsFrame: ElementRef;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  destUrl: string = 'https://72fest.com/sponsors/';
+  safeDestUrl: SafeResourceUrl;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public sanitizer: DomSanitizer,
+    public loadingCtrl: LoadingController
+  ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SponsorsPage');
-  }
+    // sanitize URL
+    this.safeDestUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.destUrl);
 
+    // create loading dialog and display it until site is loaded
+    const loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    // listen for site to finish loading
+    this.sponsorsFrame.nativeElement.addEventListener('load', event => {
+      // hide loading dialog after site is finished
+      loading.dismiss();
+    });
+  }
 }
