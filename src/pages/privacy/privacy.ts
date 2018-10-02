1d88321cc73abdf3c72e5f5da2bd62ac441e0a9c
Lonny Gomes <code@lonnygomes.com>
2018-10-01 21:03:12 -0400

    Add link to privacy statement within the menu, fix #48

--- src/pages/privacy/privacy.ts
+++ src/pages/privacy/privacy.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the PrivacyPage page.
@@ -11,15 +12,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-privacy',
-  templateUrl: 'privacy.html',
+  templateUrl: 'privacy.html'
 })
 export class PrivacyPage {
+  @ViewChild('privacyFrame')
+  privacyFrame: ElementRef;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  destUrl: string = 'http://72fest.com/about/privacy-policy/';
+  safeDestUrl: SafeResourceUrl;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public sanitizer: DomSanitizer,
+    public loadingCtrl: LoadingController
+  ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PrivacyPage');
-  }
+    // sanitize URL
+    this.safeDestUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.destUrl);
 
+    // create loading dialog and display it until site is loaded
+    const loading = this.loadingCtrl.create({
+      content: 'Please wait ...'
+    });
+
+    loading.present();
+
+    // listen for site to finish loading
+    this.privacyFrame.nativeElement.addEventListener('load', event => {
+      // hide loading dialog after site is finished
+      loading.dismiss();
+    });
+  }
 }
